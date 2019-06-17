356f01a00e97f152f50908b4d1237e2cee67e759
samundrak <samundra.khatri@javra.com>
2019-06-17 11:40:27 +0545

    First commit

---
+++ skecth.js
@@ -0,0 +1,21 @@
+const canvasSketch = require('canvas-sketch');
+
+const settings = {
+  dimensions: [2048, 2048],
+};
+
+const sketch = () => {
+  return ({ context, width, height }) => {
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, width, height);
+
+    context.beginPath();
+    context.arc(width / 2, height / 2, 300, Math.PI * 2, false);
+    context.fillStyle = 'red';
+    context.fill();
+    context.lineWidth = 40;
+    context.stroke();
+  };
+};
+
+canvasSketch(sketch, settings);
