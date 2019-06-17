d19c93d5079faee829833838d592b3cdb38c4df8
samundrak <samundra.khatri@javra.com>
2019-06-17 12:38:35 +0545

    cool grid effects

--- skecth.js
+++ skecth.js
@@ -1,20 +1,47 @@
 const canvasSketch = require('canvas-sketch');
+const { lerp } = require('canvas-sketch-util/math');
+const random = require('canvas-sketch-util/random');
 
 const settings = {
   dimensions: [2048, 2048],
 };
 
 const sketch = () => {
+  const createGrid = () => {
+    const points = [];
+    const count = 40;
+    for (let x = 0; x < count; x++) {
+      for (let y = 0; y < count; y++) {
+        const u = count <= 1 ? 0.5 : x / (count - 1);
+        const v = count <= 1 ? 0.5 : y / (count - 1);
+        points.push({
+          radius: random.value() * 0.01,
+          position: [u, v],
+        });
+      }
+    }
+    return points;
+  };
+  random.setSeed('samundra');
+  const points = createGrid().filter(() => random.value() > 0.5);
+  const margin = 400;
+
+  console.log(points);
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    context.beginPath();
-    context.arc(width / 2, height / 2, 300, Math.PI * 2, false);
-    context.fillStyle = 'red';
-    context.fill();
-    context.lineWidth = 40;
-    context.stroke();
+    points.forEach(data => {
+      const { radius, position } = data;
+      const [u, v] = position;
+
+      const x = lerp(margin, width - margin, u);
+      const y = lerp(margin, width - margin, v);
+      context.beginPath();
+      context.arc(x, y, radius * width, 0, Math.PI * 2, false);
+      context.fillStyle = 'red';
+      context.fill();
+    });
   };
 };
 
